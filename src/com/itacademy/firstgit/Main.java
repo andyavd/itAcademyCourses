b3a963e92423778906e83970e54be75fafa3b6f0
Andrei Audzeichyk <andyavd@yandex.ru>
2020-09-02 20:43:16 +0300

    Created Cat and Dog classes

--- src/com/itacademy/firstgit/Main.java
+++ src/com/itacademy/firstgit/Main.java
@@ -1,9 +1,20 @@
 package com.itacademy.firstgit;
 
+import com.itacademy.firstgit.animals.Cat;
+import com.itacademy.firstgit.animals.Dog;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-
+		Cat vaska = new Cat();
+		vaska.setAge(5);
+		vaska.setName("Vaska");
+		
+		Dog sharik = new Dog();
+		sharik.setAge(4);
+		sharik.setName("Sharik");
+		
+		System.out.println("Cats name is " + vaska.getName());
+		System.out.println("Dogs name is " + sharik.getName());
 	}
 }
